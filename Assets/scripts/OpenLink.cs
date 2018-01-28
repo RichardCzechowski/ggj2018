3ba422ec291203a1deed0eca28715044e83dabbb
Richard Czechowski <richardczechowski@gmail.com>
2018-01-28 12:32:12 -0800

    Add links, finish, make builds, ready for sharing

---
+++ Assets/scripts/OpenLink.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenLink : MonoBehaviour {
+	private Button btn;
+	private PointTracker pointTracker;
+
+	public string[] urls;
+
+	private string url;
+
+	// Awake, not Start because we reset points to 0 on Start
+	void Awake () {
+		pointTracker = GameObject.Find("PlayerManager").GetComponent<PointTracker>();
+		url = urls[pointTracker.points];
+
+		Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	void TaskOnClick ()
+	{
+        Application.OpenURL(url);
+	}
+}
